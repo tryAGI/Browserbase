f3998fe2f9620e90685f8f3e3e358cf5991c0e2f
Konstantin S. <havendv@gmail.com>
2026-08-14 19:51:35 +0400

    feat: Updated OpenAPI spec (#100)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Browserbase/Generated/Browserbase.BrowserbaseClient.g.cs
+++ src/libs/Browserbase/Generated/Browserbase.BrowserbaseClient.g.cs
@@ -34,10 +34,17 @@ namespace Browserbase
 
         /// <inheritdoc/>
         public global::Browserbase.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Browserbase.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Browserbase.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
